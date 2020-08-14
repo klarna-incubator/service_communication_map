49d4917d099f131f59c6f2754905da4e2cccb47e
Marco Denisi <marco.denisi@klarna.com>
2020-08-14 16:31:44 +0200

    when click the button, go to the service and retrieve the image

--- src/main/java/com/klarna/secoma/CommunicationMapService.java
+++ src/main/java/com/klarna/secoma/CommunicationMapService.java
@@ -8,6 +8,7 @@ public class CommunicationMapService {
 
     public StreamResource findCommunicationMap(String correlationId) {
         System.out.println(String.format("### Correlation ID: %s", correlationId));
+        // todo return a stream resource containing the actual graph
         return null;
     }
 
--- src/main/java/com/klarna/secoma/MainView.java
+++ src/main/java/com/klarna/secoma/MainView.java
@@ -1,19 +1,29 @@
 package com.klarna.secoma;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Route("")
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
@@ -31,24 +41,50 @@ public class MainView extends VerticalLayout {
         button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         button.addClickShortcut(Key.ENTER);
         button.addClickListener(event -> {
-
             String correlationId = textField.getValue();
             if (StringUtils.isEmpty(correlationId)) {
-                System.out.println("Empty correlation ID");
+                Notification.show("Empty correlation ID");
                 return;
             }
 
             UI ui = UI.getCurrent();
             CompletableFuture.supplyAsync(() -> communicationMapService.findCommunicationMap(correlationId))
                     .thenAccept(streamResource -> {
-                        ui.access(() -> {
-
-                        });
+                        Image image = (Image) ui.getChildren().filter(component -> component.getClass() == MainView.class)
+                                .findFirst()
+                                .orElseThrow()
+                                .getChildren()
+                                .filter(component -> component.getClass() == Image.class)
+                                .findFirst()
+                                .orElseThrow();
+                        image.setSrc(streamResource);
                     });
         });
+
         Image graphImage = new Image();
 
-        add(textField, button, graphImage);
+        VerticalLayout workspace = new VerticalLayout();
+        workspace.setAlignItems(Alignment.CENTER);
+        workspace.setSizeFull();
+        workspace.add(textField, button, graphImage);
+
+        setSizeFull();
+        setMargin(false);
+        setSpacing(false);
+        setPadding(false);
+        add(createHeader(), workspace);
+    }
+
+    private Component createHeader() {
+        Icon drawer = VaadinIcon.MENU.create();
+        Span title = new Span("Service Communication Map Viewer");
+        Icon help = VaadinIcon.QUESTION_CIRCLE.create();
+        HorizontalLayout header = new HorizontalLayout(drawer, title, help);
+        header.expand(title);
+        header.setPadding(true);
+        header.setWidth("100%");
+
+        return header;
     }
 
 }
