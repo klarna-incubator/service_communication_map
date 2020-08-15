a6abf3cc0b216e0304ae107055013b291ab10739
yoosiba <944287+yoosiba@users.noreply.github.com>
2020-08-15 09:14:52 +0200

    GH-6 use streaming file reader

--- src/main/java/com/klarna/secoma/LogReader.java
+++ src/main/java/com/klarna/secoma/LogReader.java
@@ -1,62 +1,40 @@
 package com.klarna.secoma;
 
-import static java.lang.String.format;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.stream.Stream;
 
 public final class LogReader {
 
 	public static Stream<String> streamLogs(Path folder) {
-		List<String> lines = new LinkedList<>();
-		readDataFolder(folder.resolve(folder), lines);
-		return lines.stream();
+		return streamAllFiles(folder);
 	}
 
-	private static void readDataFolder(Path path, List<String> data) {
-		System.out.println("\n = " + path.toAbsolutePath());
-		timed(() -> readAllFiles(path, data), "total");
-	}
-
-	private static void readAllFiles(Path dataFolder, List<String> allLines) {
-		try (Stream<Path> paths = Files.walk(dataFolder)) {
-			paths	.parallel()
-					.filter(path -> path.toFile()
-										.isFile())
-					.forEach(path -> timed(() -> readFileIntoList(path, allLines), "  - " + path.getFileName()));
+	private static Stream<String> streamAllFiles(Path dataFolder) {
+		try {
+			return Files.walk(dataFolder)
+						.parallel()
+						.filter(path -> path.toFile()
+											.isFile())
+						.flatMap(LogReader::streamFile);
 		} catch (IOException ioe) {
-			throw new RuntimeException("Cannot read " + dataFolder, ioe);
+			System.err.println("Cannot read folder " + dataFolder.toAbsolutePath() + " due to " + ioe	.getCause()
+																										.getMessage());
+			ioe.printStackTrace();
 		}
+		return Stream.empty();
 	}
 
-	private static void readFileIntoList(Path source, List<String> allLines) {
-		try (BufferedReader reader = Files.newBufferedReader(source, StandardCharsets.UTF_8)) {
-			for (;;) {
-				String line = reader.readLine();
-				if (line == null)
-					break;
-				allLines.add(line);
-			}
-		} catch (IOException ex) {
-			throw new RuntimeException("Cannot read " + source, ex);
+	private static Stream<String> streamFile(Path path) {
+		try {
+			return Files.lines(path);
+		} catch (IOException ioe) {
+			System.err.println("Cannot read file " + path.toAbsolutePath() + " due to " + ioe	.getCause()
+																								.getMessage());
+			ioe.printStackTrace();
 		}
-	}
-
-	private static void timed(Runnable runnable, String msg) {
-		Instant start = Instant.now();
-		runnable.run();
-		Instant end = Instant.now();
-		Duration duration = Duration.between(start, end);
-		System.out.println(format("%s %sd %sh %sm %ss %sms %sns", msg, duration.toDaysPart(), duration.toHoursPart(),
-				duration.toMinutesPart(), duration.toSecondsPart(), duration.toMillisPart(), duration.toNanosPart()));
+		return Stream.empty();
 	}
 
 	private LogReader() {
