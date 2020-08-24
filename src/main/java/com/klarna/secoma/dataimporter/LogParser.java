1eaa2ed5615f1969c78b189047e5ab156e756ec7
Jakub Siberski <jakub.siberski@klarna.com>
2020-08-24 20:15:10 +0200

    GH-24 add IDs extraction

--- src/main/java/com/klarna/secoma/dataimporter/LogParser.java
+++ src/main/java/com/klarna/secoma/dataimporter/LogParser.java
@@ -5,9 +5,12 @@ import static java.lang.String.format;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -34,6 +37,16 @@ public final class LogParser {
 	private static final List<Pattern> CORR_ID_PATERNS = List.of(Pattern.compile(CORR_REX_1),
 			Pattern.compile(CORR_REX_2), Pattern.compile(CORR_REX_3));
 
+	private static final String ID_NAME = "IDNAME";
+	private static final String ID_VALUE = "IDVALE";
+	private static final String IDENTIFIER = "[a-zA-Z0-0|_|-]*";
+	private static final String QUOTES= "[\"|\\\\\"]\"\"?";
+	// ["|\\"]""?(?<ID_NAME>[a-zA-Z0-0|_|-]*)["|\\"]""?:["|\\"]""?(?<ID_VALE>([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}))["|\\"]""?
+	private static final String ID_REX = format("%s(?<%s>%s)%s:%s(?<%s>%s)%s", QUOTES,ID_NAME,IDENTIFIER,QUOTES,QUOTES,ID_VALUE, UUID_GROUP,QUOTES);
+	
+	private static final Pattern IDS_PATTERN = Pattern.compile(ID_REX);
+	
+	
 	/**
 	 * assume incoming stream of CSV rows schema to be
 	 * [{@code sourcetype, "_time", "_raw"}]
@@ -58,7 +71,24 @@ public final class LogParser {
 					System.err.println("Can't find correlation id matcher for " + raw);
 					return null;
 				}
-				return new LogEntry(name.replace(DQ, NOTHING), instant, UUID.fromString(id));
+				
+				
+				Matcher matcher = IDS_PATTERN.matcher(raw);
+				Map<String, UUID> ids = new HashMap<>();
+				while(matcher.find()) {
+					String idName = matcher.group(ID_NAME);
+					UUID idValue = UUID.fromString(matcher.group(ID_VALUE));
+					UUID old = ids.put(idName, idValue);
+					if(old != null && !old.equals(idValue)) {
+						ids.put(idName, idValue);
+					}
+				}
+				
+				if (ids.isEmpty()) {
+					System.err.println("Can't find id " + raw);
+					return null;
+				}
+				return new LogEntry(name.replace(DQ, NOTHING), instant, ids, UUID.fromString(id));
 			} catch (DateTimeParseException dtpe) {
 				throw new RuntimeException("can't parse " + time, dtpe);
 			}
