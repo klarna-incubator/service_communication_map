3ab05cce8d5ea9716246c411fdf60086c7034833
yoosiba <944287+yoosiba@users.noreply.github.com>
2020-09-09 01:43:26 +0200

    add simple parsing test

---
+++ src/test/java/com/klarna/secoma/LogParserTest.java
@@ -0,0 +1,41 @@
+/*
+ */
+package com.klarna.secoma;
+
+import static com.klarna.secoma.dataimporter.LogParser.parse;
+import static com.klarna.secoma.dataimporter.LogReader.streamLogs;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import com.klarna.secoma.dataimporter.LogEntry;
+
+
+public class LogParserTest {
+	
+	@Test
+	public void canParseSampleData() {
+		String resource = this.getClass().getResource("/single_flow.csv").getPath();
+		List<LogEntry> logs = loadData(resource);
+		int logLinesCount = 39; // log lines in CSV (not header, not last empty line)
+		assertThat(logs.size(),  is(equalTo(logLinesCount)));
+	}
+	
+	
+	private static List<LogEntry> loadData(String path) {
+		System.err.println("loading " + path);
+		Path workDir = Paths.get(path);
+		Stream<LogEntry> logs = parse(streamLogs(workDir));
+		return logs.collect(Collectors.toList());
+	}
+
+}
