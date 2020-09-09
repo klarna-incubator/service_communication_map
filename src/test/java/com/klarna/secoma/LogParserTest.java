aff0f0173a4695680c693ef2927681857dae4d55
yoosiba <944287+yoosiba@users.noreply.github.com>
2020-09-09 16:38:56 +0200

    use class loader

--- src/test/java/com/klarna/secoma/LogParserTest.java
+++ src/test/java/com/klarna/secoma/LogParserTest.java
@@ -8,7 +8,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -19,23 +18,28 @@ import org.junit.Test;
 
 import com.klarna.secoma.dataimporter.LogEntry;
 
-
 public class LogParserTest {
-	
+
 	@Test
 	public void canParseSampleData() {
-		String resource = this.getClass().getResource(Paths.get("/single_flow.csv").toString()).getPath();
-		List<LogEntry> logs = loadData(resource);
+		List<LogEntry> logs = loadData(getFixture("single_flow.csv"));
 		int logLinesCount = 39; // log lines in CSV (not header, not last empty line)
 		assertThat(logs.size(),  is(equalTo(logLinesCount)));
 	}
-	
-	
+
 	private static List<LogEntry> loadData(String path) {
 		System.err.println("loading " + path);
 		Path workDir = Paths.get(path);
 		Stream<LogEntry> logs = parse(streamLogs(workDir));
 		return logs.collect(Collectors.toList());
+	}
+
+	private String getFixture(String... more) {
+		return getResource("fixture", more);
+	}	
+
+	private String getResource(String first, String... more) {
+		return this.getClass().getClassLoader().getResource(Paths.get(first, more).toString()).getPath();
 	}
 
 }
